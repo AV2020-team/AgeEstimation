20e30248e0cb31c0eb3e66c18345a1497644690a
Vincenzo Petrone <v.petrone6@studenti.unisa.it>
2021-01-07 19:22:18 +0100

    Update start_train.py.sh

--- training/start_train.py.sh
+++ training/start_train.py.sh
@@ -6,13 +6,13 @@ pip install omegaconf
 pip install dlib
 
 python3 train.py \
---net efficientnetb3 \
+--net vgg16 \
 --dataset vggface2_age \
---pretraining EfficientNetB3_224_weights.11-3.44.hdf5 \
+--pretraining imagenet \
 --preprocessing full_normalization \
 --augmentation myautoaugment \
---batch 64 \
---lr 0.005:0.2:20 \
+--batch 128 \
+--lr 0.005:0.5:5 \
 --sel_gpu 0 \
 --training-epochs 70 \
 --weight_decay 0.005 \
